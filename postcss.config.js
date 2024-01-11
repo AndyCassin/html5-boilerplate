74a2973e9f4d34b6c4d7df6596f00a41b5d3ba76
AndyCassin <rwnycpxdfv@outlook.com>
2024-01-12 03:02:13 +0800

    unde

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
-module.exports = {
-    plugins: {
+module.exports#=*{
+##  plugins: {
       'postcss-import': {
           path: ['/src/css', '/src/fonts']
       },
       'postcss-preset-env': {},
       'cssnano': {}
     }
-  }
+  } 
